6bc85999fff0f803da6e1cff1a936d07c40c6dcb
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-13 01:05:17 +0000

    add all helper functions and eslint

--- helpers.js
+++ helpers.js
@@ -1,6 +1,4 @@
-
-
-const getUserByEmail = function (email, database) {
+const getUserByEmail = function(email, database) {
   for (const user in database) {
     if (database[user].email === email) {
       return user; // true
@@ -9,5 +7,28 @@ const getUserByEmail = function (email, database) {
   return undefined;
 };
 
+//function to check if user 
+const urlsForUser = function(id, urlDatabase) {
+  const userURLs = {};
+  if (!id) {
+    return null;
+  }
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      userURLs[key] = urlDatabase[key];
+    }
+  }
+  return userURLs;
+};
 
-module.exports = {getUserByEmail}
+//random sting generator
+const generateRandomString = function(length) {
+  const elements = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const elementsLength = elements.length;
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    randomString += elements.charAt(Math.floor(Math.random() * elementsLength));
+  } return randomString;
+};
+
+module.exports = {getUserByEmail, urlsForUser, generateRandomString};
